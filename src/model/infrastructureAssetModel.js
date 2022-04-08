e4086ac867d860d2ac75c6d6fb404f4cef9136cf
ElLettuce <81658867+ElLettuce@users.noreply.github.com>
2022-04-08 00:04:44 -0500

    POST methods for importing assets

--- src/model/infrastructureAssetModel.js
+++ src/model/infrastructureAssetModel.js
@@ -4,9 +4,12 @@ let workbook = XLSX.readFile('./src/data/Infrastructure.xlsx', {type: "binary"})
 let data = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
 
 let infrastructureAssetModel = {
-    
+
+    push: (asset) =>{
+        data.push(...asset);
+    },
     findById: (id) => {
-        return data.filter(item => parseInt(item["Infrastructure ID"]) == id)[0];
+        return data.filter(item => parseInt(item["Infrastructure ID"]) === id)[0];
     },
     findAll: () => {
         return data;
